4fa4536cbbcbfc7d51378e82a9bf3e8d58d534bf
Mo <31892724+mo-rob-1@users.noreply.github.com>
2021-05-05 17:18:08 +0100

    update

--- js/script.js
+++ js/script.js
@@ -3,3 +3,22 @@ $(document).ready(function () {
     $("#nav-panel").slideToggle("slow");
   });
 });
+
+$(document).ready(function () {
+  $(".button").click(function () {
+    var value = $(this).attr("data-filter");
+    if (value == "all") {
+      $(".filter").show("1000");
+    } else {
+      $(".filter")
+        .not("." + value)
+        .hide("1000");
+      $(".filter")
+        .filter("." + value)
+        .show("1000");
+    }
+    $(".pricing__btn-container .button").click(function () {
+      $(this).addClass("active");
+    });
+  });
+});
